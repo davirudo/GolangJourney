a8352173cbadb21f92d17bc7e1282ab8830b4408
David Diamanta <dantadiamanta@gmail.com>
2024-02-03 10:36:51 +0700

    int bool and string

---
+++ sample.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Int
+	fmt.Println("One = ", 1)
+	fmt.Println("Two = ", 2)
+	fmt.Println("Two poin five = ", 2.5)
+	fmt.Println()
+	//Boolean
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+	fmt.Println()
+	//String
+	fmt.Println("Davirudo")
+	fmt.Println(len("Davirudo"))
+	fmt.Println("Davirudo"[0])
+	fmt.Println("Davirudo"[1])
+
+}
