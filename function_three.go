da1e6040e4d79be334952cd94c8219e11d7531e4
David Diamanta <dantadiamanta@gmail.com>
2024-02-15 18:46:49 +0700

    fucntion type declarations

--- function_three.go
+++ function_three.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //Function as Paramter
 //ada function didalam function
 func helloWithFilter(name string, filter func(string) string) {
@@ -14,10 +16,31 @@ func spamFilter(name string) string {
 	}
 }
 
+//Function type Declarations
+//membuat alias agar tidak panjang
+type Filter func(string) string
+
+func helloWithFiltertype(name string, filter Filter) {
+	fmt.Println("Hello", filter(name))
+}
+
+func spamFiltertype(name string) string {
+	if name == "Budi" {
+		return "tak dikenal"
+	} else {
+		return name
+	}
+}
+
 func main() {
 	//Function as Paramter
 	helloWithFilter("David", spamFilter)
 	filter := spamFilter
 	helloWithFilter("Budi", filter)
+
+	//Function type declarations
+	helloWithFiltertype("David", spamFiltertype)
+	filtertype := spamFiltertype
+	helloWithFiltertype("Budi", filtertype)
 
 }
