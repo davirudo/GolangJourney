43c3b5761535b58701c7b16e8326a5be87b2cfb9
David Diamanta <dantadiamanta@gmail.com>
2024-02-17 00:12:32 +0700

    recover (right version)

--- deferpanicrecover.go
+++ deferpanicrecover.go
@@ -30,27 +30,41 @@ func runApp2(error bool) {
 }
 
 //Recover
-//untuk menangkap data panic
+//untuk menangkap data panic, dan panic akan dihentikan lalu data dijalankan kembali
+//contoh recover yang salah
+func runApp3salah(error bool) {
+	defer endApp()
+	if error {
+		panic("ERROR") //akan berhenti disini
+	}
+	message := recover()
+	fmt.Println("Terjadi Panic", message)
+}
+//contoh recover yang benar
+func endAppRecover() {
+	fmt.Println("End App")
+	message := recover()
+	fmt.Println("Terjadi Panic", message)
+}
+
 func runApp3(error bool) {
 	defer endApp()
 	if error {
 		panic("ERROR")
 	}
-	message := recover()
-	fmt.Println("Terjadi Error", message)
-}
+} //dimasukkan ke defer karena walau error code tetap di eksekusi
 
 
 
 func main() {
-	//Defer
+	// Defer
 	runApp()
 
 	//Panic
-	runApp2(false)
-	runApp2(true)
+	runApp2(false) //jika true, tidak bisa mengeksekusi code yang dibawah line ini karena apanic
 
-	//Recover
-	runApp3(true)
+	// Recover
+	runApp3(false)
+	fmt.Println("Halo")
 	
 }
