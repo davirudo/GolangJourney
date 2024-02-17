debb46bfbf2eff22f1c1a1c0ebb7e71b2a30a586
David Diamanta <dantadiamanta@gmail.com>
2024-02-18 04:40:25 +0700

    pointer

---
+++ pointer.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Prov, Country string
+}
+
+func main() {
+	//Pass by Value
+	//saat kirim variabel ke  func, method, var lain. data awal tidak berubah
+	address1 := Address{"Jogja", "Jawa", "Indo"}
+	address2 := address1 //duplikat value
+
+	address2.City = "Bandung"
+	fmt.Println(address1) //data tidak berubah walau dirubah Bandung di address2 
+	fmt.Println(address2)
+	fmt.Println()
+
+
+	//Pass by Reference (Pointer)
+	//data akan berubah langsung ke variabel yang dikirim
+	address3 := Address{"Jogja", "Jawa", "Indo"}
+	address4 := &address3 //duplikat value
+
+	fmt.Println(address4)
+	address4.City = "Bandung"
+	fmt.Println(address3) //data berubah walau dirubah Bandung di address2 
+	fmt.Println(address4)
+	}
+
