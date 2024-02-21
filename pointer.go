4b52dcebc71fe09b776df96d153ffbc0d1a59ca1
David Diamanta <dantadiamanta@gmail.com>
2024-02-21 21:10:28 +0700

    make it clear

--- pointer.go
+++ pointer.go
@@ -12,18 +12,19 @@ func main() {
 	address1 := Address{"Jogja", "Jawa", "Indo"}
 	address2 := address1 //duplikat value
 
+	fmt.Println(address2) //sebelum
 	address2.City = "Bandung"
 	fmt.Println(address1) //data tidak berubah walau dirubah Bandung di address2 
 	fmt.Println(address2)
 	fmt.Println()
 
 
-	//Pass by Reference (Pointer)
+	//Pass by Reference (Pointer "&")
 	//data akan berubah langsung ke variabel yang dikirim
 	address3 := Address{"Jogja", "Jawa", "Indo"}
 	address4 := &address3 //duplikat value
 
-	fmt.Println(address4)
+	fmt.Println(address4) //sebelum
 	address4.City = "Bandung"
 	fmt.Println(address3) //data berubah walau dirubah Bandung di address2 
 	fmt.Println(address4)
