9da8f8ce2559a584634582129a755217111150f5
David Diamanta <dantadiamanta@gmail.com>
2024-02-28 22:00:04 +0700

    add comment

--- standarLibrary/regexp.go
+++ standarLibrary/regexp.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-//Mencocokan sesuatu regexp yg di compile
+//Mencocokan berdasarkan format yg diinginkan
 func main() {
 	//MustCompile, membuat regexp
 	//MatchString, is Regexp match with string?
@@ -13,9 +13,9 @@ func main() {
 
 	var regex = regexp.MustCompile(`d([a-z])v`) //tambahkan ([a-z]) lagi jika lebih dari 3 huruf
 
-	fmt.Println(regex.MatchString("david"))
-	fmt.Println(regex.MatchString("dav"))
-	fmt.Println(regex.MatchString("David"))
+	fmt.Println(regex.MatchString("david")) //dav masuk
+	fmt.Println(regex.MatchString("dov"))
+	fmt.Println(regex.MatchString("David")) //D besar
 
 	fmt.Println(regex.FindAllString("dav david David Dav dov davirudo", 10))
 }
