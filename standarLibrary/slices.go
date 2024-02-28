58078ae3413ece61fcc0e35f895fe91393341221
David Diamanta <dantadiamanta@gmail.com>
2024-02-28 22:40:20 +0700

    slices int

--- standarLibrary/slices.go
+++ standarLibrary/slices.go
@@ -5,11 +5,14 @@ import (
 	"slices"
 )
 
-//Manipulate slice
+//Mengecek dan Manipulate slice
 func main() {
 	names := []string{"John", "Paul", "George", "Ringo"}
+	values := []int{100, 90, 80, 70}
 
 	fmt.Println(slices.Contains(names, "Paul"))
 	fmt.Println(slices.Contains(names, "Dino"))
 	fmt.Println(slices.Index(names, "George"))
+	fmt.Println(slices.Min(values))
+	fmt.Println(slices.Max(values))
 }
